e4cef5b5c92e55b175890d0679f9c9cc27c4c08e
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-12 22:32:29 -0300

    add math para params de rota dinamico

---
+++ src/params/subject.ts
@@ -0,0 +1,19 @@
+import type { ParamMatcher } from "@sveltejs/kit"
+
+function matchSubject(subject: string) {
+  switch(subject.toLowerCase()) {
+    case "html":
+      return true;
+    case "css":
+      return true;
+    case "javascript":
+      return true;
+    case "accessibility":
+      return true;
+  }
+  return false;
+}
+
+export const match: ParamMatcher = (param) => {
+  return matchSubject(param)
+}

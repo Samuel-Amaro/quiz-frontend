0a4751c0bb99ff7589e7396cf73d1682038389d5
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-12 22:35:08 -0300

    add function util para obter icon de svg componente

---
+++ src/lib/util/utils.ts
@@ -0,0 +1,31 @@
+import Html from "$lib/icons/Html.svelte";
+import Css from "$lib/icons/Css.svelte";
+import Javascript from "$lib/icons/Javascript.svelte";
+import Acessibility from "$lib/icons/Acessibility.svelte";
+
+export function getDatasViewSubject(subject: string) {
+  const datasViewSubject = [
+    {
+      subject: "html",
+      class: "html",
+      component: Html
+    },
+    {
+      subject: "css",
+      class: "css",
+      component: Css
+    },
+    {
+      subject: "javascript",
+      class: "javascript",
+      component: Javascript
+    },
+    {
+      subject: "accessibility",
+      class: "accessibility",
+      component: Acessibility
+    }
+  ];
+  const index = datasViewSubject.findIndex((data) => data.subject === subject.toLowerCase())
+  return datasViewSubject[index];
+}

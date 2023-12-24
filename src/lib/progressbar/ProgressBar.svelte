e848642e400a30e262158f58e0f6b9e844825338
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-24 15:55:42 -0300

    feat: adiciona componente `ProgressBar`

---
+++ src/lib/progressbar/ProgressBar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let min: number;
+	export let max: number;
+	export let value: number;
+
+	function calculatePercentFill(min: number, max: number, value: number) {
+		return (100 * (value - min)) / (max - min);
+	}
+</script>
+
+<div
+	role="progressbar"
+	aria-label="Progresso do quiz"
+	aria-valuenow={value}
+	aria-valuemin={min}
+	aria-valuemax={max}
+>
+	<span style="width: {calculatePercentFill(min, max, value)}%;"></span>
+</div>
+
+<style>
+	div {
+		height: 16px;
+		padding: 4px;
+		border-radius: 999px;
+		background-color: var(--choose03);
+		overflow: hidden;
+	}
+
+	span {
+		display: block;
+		background-color: var(--purple);
+		border-radius: 999px;
+		height: 100%;
+	}
+</style>

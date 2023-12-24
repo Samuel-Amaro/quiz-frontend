3d8017f623c43831de866d6af633849063fc7dcf
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-24 15:53:29 -0300

    feat: add `script theme js`

--- static/theme.js
+++ static/theme.js
@@ -1,10 +1,10 @@
-const themeSaved = localStorage.getItem("theme");
+const themeSaved = localStorage.getItem('theme');
 
-if(themeSaved) {
-  document.body.setAttribute("data-theme", themeSaved);
-}else{
-  const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
-  const theme = prefersLight ? "light" : "dark";
-  document.body.setAttribute("data-theme", theme);
-  localStorage.setItem("theme", theme);
+if (themeSaved) {
+	document.body.setAttribute('data-theme', themeSaved);
+} else {
+	const prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
+	const theme = prefersLight ? 'light' : 'dark';
+	document.body.setAttribute('data-theme', theme);
+	localStorage.setItem('theme', theme);
 }

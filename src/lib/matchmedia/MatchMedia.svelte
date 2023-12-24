4415ec7f67b75e7cdbccec7fcd6e7ee2c02cf48a
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-24 15:55:04 -0300

    feat: adiciona componente `MatchMedia`

--- src/lib/matchmedia/MatchMedia.svelte
+++ src/lib/matchmedia/MatchMedia.svelte
@@ -1,36 +1,35 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-  export let mediaQuery: string
+	export let mediaQuery: string;
 
-  let isScreenCurrentMatch = false
+	let isScreenCurrentMatch = false;
 
-  function updateIsScreenCurrentMatch(e: MediaQueryListEvent) {
-    isScreenCurrentMatch = e.matches
-  }
+	function updateIsScreenCurrentMatch(e: MediaQueryListEvent) {
+		isScreenCurrentMatch = e.matches;
+	}
 
-  onMount(() => {
-    const mediaWatcher = window.matchMedia(mediaQuery);
-    isScreenCurrentMatch = mediaWatcher.matches
+	onMount(() => {
+		const mediaWatcher = window.matchMedia(mediaQuery);
+		isScreenCurrentMatch = mediaWatcher.matches;
 
-    if(mediaWatcher.addEventListener) {
-      mediaWatcher.addEventListener("change", updateIsScreenCurrentMatch)
+		if (mediaWatcher.addEventListener) {
+			mediaWatcher.addEventListener('change', updateIsScreenCurrentMatch);
 
-      return () => {
-        mediaWatcher.removeEventListener("change", updateIsScreenCurrentMatch);
-      }
-    }else{
-      mediaWatcher.addListener(updateIsScreenCurrentMatch)
-      return () => {
-        mediaWatcher.removeListener(updateIsScreenCurrentMatch)
-      }
-    }
-  })
-
+			return () => {
+				mediaWatcher.removeEventListener('change', updateIsScreenCurrentMatch);
+			};
+		} else {
+			mediaWatcher.addListener(updateIsScreenCurrentMatch);
+			return () => {
+				mediaWatcher.removeListener(updateIsScreenCurrentMatch);
+			};
+		}
+	});
 </script>
 
 {#if isScreenCurrentMatch}
-  <slot name="desktop"/>
-  {:else}
-    <slot name="mobile"/>
+	<slot name="desktop" />
+{:else}
+	<slot name="mobile" />
 {/if}

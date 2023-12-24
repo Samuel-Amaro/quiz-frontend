e0d5004475064b2d5fed30c26c7072efe39c63ef
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-24 16:08:43 -0300

    feat: adiciona `store theme`

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -2,17 +2,18 @@ import { writable } from 'svelte/store';
 import type { Theme } from '$lib/types/Theme';
 import { browser } from '$app/environment';
 
-let themeSaved: Theme | undefined =  undefined;
+let themeSaved: Theme | undefined = undefined;
 
-if(browser) {
-  themeSaved = localStorage.getItem("theme") !== null ? (localStorage.getItem("theme") as Theme) : 'light';
+if (browser) {
+	themeSaved =
+		localStorage.getItem('theme') !== null ? (localStorage.getItem('theme') as Theme) : 'light';
 }
 
 export const theme = writable<Theme>(themeSaved);
 
 theme.subscribe((value) => {
-  if(browser) {
-    localStorage.setItem("theme", value);
-    document.body.setAttribute("data-theme", value);
-  }
+	if (browser) {
+		localStorage.setItem('theme', value);
+		document.body.setAttribute('data-theme', value);
+	}
 });

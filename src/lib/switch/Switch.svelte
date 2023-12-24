a684f3362828d91ac53975082e577da6882722de
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-24 15:54:38 -0300

    feat: adiciona componente `Switch`

--- src/lib/switch/Switch.svelte
+++ src/lib/switch/Switch.svelte
@@ -14,7 +14,7 @@
 	) {
 		const checked = e.currentTarget.getAttribute('aria-checked') === 'true' ? true : false;
 		theme.update(() => (checked ? 'light' : 'dark'));
-		e.currentTarget.setAttribute("aria-checked", checked ? 'false' : 'true');
+		e.currentTarget.setAttribute('aria-checked', checked ? 'false' : 'true');
 	}
 
 	function handleClick(
@@ -37,12 +37,12 @@
 </script>
 
 <div class="container">
-	<span class="icon {$theme === "light" ? "light" : "dark"}">
+	<span class="icon {$theme === 'light' ? 'light' : 'dark'}">
 		<Sunny />
 	</span>
 	<div
 		role="switch"
-		aria-checked={$theme === "light" ? "false" : "true"}
+		aria-checked={$theme === 'light' ? 'false' : 'true'}
 		on:click={(e) => handleClick(e)}
 		on:keydown={(e) => handleKeydown(e)}
 		tabindex={0}
@@ -52,62 +52,61 @@
 	>
 		<span class="controller"></span>
 	</div>
-	<span class="icon {$theme === "light" ? "light" : "dark"}">
+	<span class="icon {$theme === 'light' ? 'light' : 'dark'}">
 		<Moon />
 	</span>
 </div>
 
-
 <style>
-  .switch{
-    background-color: var(--purple);
-    width: 32px;
-    padding: 4px;
-    height: 20px;
-    border-radius: 999px;
-    transition: all 0.3s ease-in-out;
-    position: relative;
-		content: "";
-  }
+	.switch {
+		background-color: var(--purple);
+		width: 32px;
+		padding: 4px;
+		height: 20px;
+		border-radius: 999px;
+		transition: all 0.3s ease-in-out;
+		position: relative;
+		content: '';
+	}
 
-  .controller{
-    display: block;
-    width: 12px;
-    height: 12px;
-    content: "";
-    border-radius: 50%;
-    background-color: var(--pure-white);
-    position: absolute;
+	.controller {
+		display: block;
+		width: 12px;
+		height: 12px;
+		content: '';
+		border-radius: 50%;
+		background-color: var(--pure-white);
+		position: absolute;
 		top: 4px;
 		left: 4px;
 		transition: all 0.35s ease;
-  }
-
-  .switch[aria-checked="false"] .controller{
-    left: 4px;
-  }
-
-  .switch[aria-checked="true"] .controller{
-    left: 16px;
-  }
-
-	.icon{
-		display: flex;
-		align-items: center;
-		color: #626C7F;
 	}
 
-	.light{
+	.switch[aria-checked='false'] .controller {
+		left: 4px;
+	}
+
+	.switch[aria-checked='true'] .controller {
+		left: 16px;
+	}
+
+	.icon {
+		display: flex;
+		align-items: center;
+		color: #626c7f;
+	}
+
+	.light {
 		color: var(--grey-navy);
 		fill: var(--grey-navy);
 	}
 
-	.dark{
+	.dark {
 		color: var(--pure-white);
 		fill: var(--pure-white);
 	}
 
-	.container{
+	.container {
 		display: flex;
 		flex-flow: row nowrap;
 		align-items: center;

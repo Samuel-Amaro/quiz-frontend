d5f1a1779bfcbc29a04d1cc25bf342d6ddfb04ea
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-25 11:02:59 -0300

    feat: adiciona `+layout.svelte` global

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../styles/index.css';
+	import { theme } from '$lib/stores/theme';
+</script>
+
+<svelte:head>
+	<meta name="color-scheme" content={$theme} />
+</svelte:head>
+
+<slot />

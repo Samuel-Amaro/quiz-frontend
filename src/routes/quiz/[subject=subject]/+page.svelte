aa0826e4666c274503b3a912aef1f96a298e14d0
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-12-26 10:10:17 -0300

    feat: add load client with ui optimistic

--- src/routes/quiz/[subject=subject]/+page.svelte
+++ src/routes/quiz/[subject=subject]/+page.svelte
@@ -6,8 +6,10 @@
 	import Error from '$lib/icons/Error.svelte';
 	import CheckMark from '$lib/icons/CheckMark.svelte';
 	import { page } from '$app/stores';
+	import type { Quiz } from '../../../types/api.type.js';
+	import type { PageData } from './$types.js';
 
-	export let data;
+	export let data: PageData;
 
 	let idxQuestion = 0;
 	let points: number = 0;
@@ -55,104 +57,102 @@
 	function handleBtnOption(idx: number) {
 		idxOptionSelected = idx;
 	}
+
+	async function getQuiz() {
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		const response = await fetch(`/api/quiz/${$page.params.subject}`);
+		const quiz: Quiz = await response.json();
+		return quiz;
+	}
 </script>
 
-<section>
-	<header>
-		<div class="headerContainer">
-			<span class="icon {datasRendering.class}">
-				<MatchMedia mediaQuery="(min-width: 400px)">
-					<svelte:component this={datasRendering.component} layout="mobile" slot="mobile" />
-					<svelte:component this={datasRendering.component} layout="desktop" slot="desktop" />
-				</MatchMedia>
-			</span>
-			<h1>{data.title}</h1>
-		</div>
-		<Switch />
-	</header>
-	<main>
-		{#if !isShowScore}
-			<div class="content">
-				<p class="contentControllerQuestions">
-					Pergunta {idxQuestion + 1} de {data.questions.length}
-				</p>
-				<h2 class="contentTitle">{question.question}</h2>
-				<ProgressBar min={0} max={data.questions.length} value={idxQuestion + 1} />
+{#await getQuiz()}
+	<div class="load">
+		<h1 class="loadTitle">Carrregando quiz {$page.params.subject.toUpperCase()}...</h1>
+	</div>
+{:then quiz}
+	<section>
+		<header>
+			<div class="headerContainer">
+				<span class="icon {datasRendering.class}">
+					<MatchMedia mediaQuery="(min-width: 400px)">
+						<svelte:component this={datasRendering.component} layout="mobile" slot="mobile" />
+						<svelte:component this={datasRendering.component} layout="desktop" slot="desktop" />
+					</MatchMedia>
+				</span>
+				<h1>{data.title}</h1>
 			</div>
-			<div class="quiz">
-				<div class="quizListOptions">
-					{#each question.options as option, i}
-						<button
-							type="button"
-							on:click={() => handleBtnOption(i)}
-							on:keydown={(e) => {
-								if (e.key === 'Enter' || e.key === '') handleBtnOption(i);
-							}}
-							title={option}
-							aria-label={`Opção: ${option}`}
-							class="quizOption"
-							class:optionCorrect={typeof idxResponse === 'number' &&
-							typeof idxOptionSelected === 'number' &&
-							idxResponse === idxOptionSelected &&
-							idxOptionSelected === i &&
-							isSubmitResponse
-								? true
-								: false}
-							class:optionActive={typeof idxOptionSelected === 'number' &&
-							idxOptionSelected === i &&
-							!isSubmitResponse
-								? true
-								: false}
-							class:optionIncorrect={typeof idxOptionSelected === 'number' &&
-							typeof idxResponse === 'number' &&
-							idxOptionSelected !== idxResponse &&
-							idxOptionSelected === i &&
-							isSubmitResponse
-								? true
-								: false}
-							disabled={isSubmitResponse}
-						>
-							<div class="quizOptionWrapper">
-								<span class="quizOrderOption">
-									<h3 class="quizTitle">{alphabet[i]}</h3>
-								</span>
-								<span class="quizContentOption">{option}</span>
-							</div>
-							<span class="quizOptionIcon">
-								{#if typeof idxResponse === 'number' && typeof idxOptionSelected === 'number' && idxResponse === i && isSubmitResponse}
-									<MatchMedia mediaQuery="(min-width: 400px)">
-										<svelte:component this={CheckMark} slot="mobile" />
-										<svelte:component this={CheckMark} size={40} slot="desktop" />
-									</MatchMedia>
-								{:else if typeof idxOptionSelected === 'number' && typeof idxResponse === 'number' && idxOptionSelected !== idxResponse && idxOptionSelected === i && isSubmitResponse}
-									<MatchMedia mediaQuery="(min-width: 400px)">
-										<svelte:component this={Error} slot="mobile" />
-										<svelte:component this={Error} size={40} slot="desktop" />
-									</MatchMedia>
-								{/if}
-							</span>
-						</button>
-					{/each}
+			<Switch />
+		</header>
+		<main>
+			{#if !isShowScore}
+				<div class="content">
+					<p class="contentControllerQuestions">
+						Pergunta {idxQuestion + 1} de {data.questions.length}
+					</p>
+					<h2 class="contentTitle">{question.question}</h2>
+					<ProgressBar min={0} max={data.questions.length} value={idxQuestion + 1} />
 				</div>
-				<button
-					type="button"
-					title="Enviar resposta"
-					aria-label="Enviar resposta"
-					on:click={() => {
-						if (
-							isSubmitResponse &&
-							!isNoResponseSelected &&
-							idxQuestion <= data.questions.length - 1
-						) {
-							nextQuestion();
-						} else if (!isSubmitResponse && !isNoResponseSelected) {
-							handleBtnSubmitResponse();
-						} else {
-							handleBtnSubmitResponse();
-						}
-					}}
-					on:keydown={(e) => {
-						if (e.key === 'Enter' || e.key === '') {
+				<div class="quiz">
+					<div class="quizListOptions">
+						{#each question.options as option, i}
+							<button
+								type="button"
+								on:click={() => handleBtnOption(i)}
+								on:keydown={(e) => {
+									if (e.key === 'Enter' || e.key === '') handleBtnOption(i);
+								}}
+								title={option}
+								aria-label={`Opção: ${option}`}
+								class="quizOption"
+								class:optionCorrect={typeof idxResponse === 'number' &&
+								typeof idxOptionSelected === 'number' &&
+								idxResponse === idxOptionSelected &&
+								idxOptionSelected === i &&
+								isSubmitResponse
+									? true
+									: false}
+								class:optionActive={typeof idxOptionSelected === 'number' &&
+								idxOptionSelected === i &&
+								!isSubmitResponse
+									? true
+									: false}
+								class:optionIncorrect={typeof idxOptionSelected === 'number' &&
+								typeof idxResponse === 'number' &&
+								idxOptionSelected !== idxResponse &&
+								idxOptionSelected === i &&
+								isSubmitResponse
+									? true
+									: false}
+								disabled={isSubmitResponse}
+							>
+								<div class="quizOptionWrapper">
+									<span class="quizOrderOption">
+										<h3 class="quizTitle">{alphabet[i]}</h3>
+									</span>
+									<span class="quizContentOption">{option}</span>
+								</div>
+								<span class="quizOptionIcon">
+									{#if typeof idxResponse === 'number' && typeof idxOptionSelected === 'number' && idxResponse === i && isSubmitResponse}
+										<MatchMedia mediaQuery="(min-width: 400px)">
+											<svelte:component this={CheckMark} slot="mobile" />
+											<svelte:component this={CheckMark} size={40} slot="desktop" />
+										</MatchMedia>
+									{:else if typeof idxOptionSelected === 'number' && typeof idxResponse === 'number' && idxOptionSelected !== idxResponse && idxOptionSelected === i && isSubmitResponse}
+										<MatchMedia mediaQuery="(min-width: 400px)">
+											<svelte:component this={Error} slot="mobile" />
+											<svelte:component this={Error} size={40} slot="desktop" />
+										</MatchMedia>
+									{/if}
+								</span>
+							</button>
+						{/each}
+					</div>
+					<button
+						type="button"
+						title="Enviar resposta"
+						aria-label="Enviar resposta"
+						on:click={() => {
 							if (
 								isSubmitResponse &&
 								!isNoResponseSelected &&
@@ -164,65 +164,89 @@
 							} else {
 								handleBtnSubmitResponse();
 							}
-						}
-					}}
-					class="quizBtnResponse"
-					class:spacing={isNoResponseSelected}
-					>{isSubmitResponse && idxQuestion < data.questions.length - 1
-						? 'Próxima Questão'
-						: isSubmitResponse && idxQuestion === data.questions.length - 1
-						  ? 'Ver Pontuação'
-						  : 'Enviar Resposta'}</button
-				>
-				{#if isNoResponseSelected && isSubmiteResponseWithoutOptionSelected}
-					<p role="alert" class="error">
-						<MatchMedia mediaQuery="(min-width: 400px)">
-							<svelte:component this={Error} slot="mobile" />
-							<svelte:component this={Error} size={40} slot="desktop" />
-						</MatchMedia>
-						<span class="errorText"> Selecione uma resposta </span>
-					</p>
-				{/if}
-			</div>
-		{:else}
-			<div class="scored">
-				<h2 class="scoredTitle">
-					Questionário concluído
-					<span class="scoredTitleColor">Você pontuou...</span>
-				</h2>
-				<div>
-					<div class="scoredContainer">
-						<div class="scoredContainerQuiz">
-							<span class="icon {datasRendering.class}">
-								<MatchMedia mediaQuery="(min-width: 400px)">
-									<svelte:component this={datasRendering.component} layout="mobile" slot="mobile" />
-									<svelte:component
-										this={datasRendering.component}
-										layout="desktop"
-										slot="desktop"
-									/>
-								</MatchMedia>
-							</span>
-							<h3 class="scoredContainerTitle">{data.title}</h3>
-						</div>
-						<h4 class="scoredContainerPoints">{points}</h4>
-						<h5 class="scoredContainerFrom">de {data.questions.length}</h5>
-					</div>
-					<a
-						href={$page.url.href}
-						target="_self"
-						rel="next"
-						title="Jogar de novo"
-						aria-label="Jogar de novo"
-						class="scoredContainerBtn"
+						}}
+						on:keydown={(e) => {
+							if (e.key === 'Enter' || e.key === '') {
+								if (
+									isSubmitResponse &&
+									!isNoResponseSelected &&
+									idxQuestion <= data.questions.length - 1
+								) {
+									nextQuestion();
+								} else if (!isSubmitResponse && !isNoResponseSelected) {
+									handleBtnSubmitResponse();
+								} else {
+									handleBtnSubmitResponse();
+								}
+							}
+						}}
+						class="quizBtnResponse"
+						class:spacing={isNoResponseSelected}
+						>{isSubmitResponse && idxQuestion < data.questions.length - 1
+							? 'Próxima Questão'
+							: isSubmitResponse && idxQuestion === data.questions.length - 1
+							  ? 'Ver Pontuação'
+							  : 'Enviar Resposta'}</button
 					>
-						Jogar de novo
-					</a>
+					{#if isNoResponseSelected && isSubmiteResponseWithoutOptionSelected}
+						<p role="alert" class="error">
+							<MatchMedia mediaQuery="(min-width: 400px)">
+								<svelte:component this={Error} slot="mobile" />
+								<svelte:component this={Error} size={40} slot="desktop" />
+							</MatchMedia>
+							<span class="errorText"> Selecione uma resposta </span>
+						</p>
+					{/if}
 				</div>
-			</div>
-		{/if}
-	</main>
-</section>
+			{:else}
+				<div class="scored">
+					<h2 class="scoredTitle">
+						Questionário concluído
+						<span class="scoredTitleColor">Você pontuou...</span>
+					</h2>
+					<div>
+						<div class="scoredContainer">
+							<div class="scoredContainerQuiz">
+								<span class="icon {datasRendering.class}">
+									<MatchMedia mediaQuery="(min-width: 400px)">
+										<svelte:component
+											this={datasRendering.component}
+											layout="mobile"
+											slot="mobile"
+										/>
+										<svelte:component
+											this={datasRendering.component}
+											layout="desktop"
+											slot="desktop"
+										/>
+									</MatchMedia>
+								</span>
+								<h3 class="scoredContainerTitle">{data.title}</h3>
+							</div>
+							<h4 class="scoredContainerPoints">{points}</h4>
+							<h5 class="scoredContainerFrom">de {data.questions.length}</h5>
+						</div>
+						<a
+							href={$page.url.href}
+							target="_self"
+							rel="next"
+							title="Jogar de novo"
+							aria-label="Jogar de novo"
+							class="scoredContainerBtn"
+						>
+							Jogar de novo
+						</a>
+					</div>
+				</div>
+			{/if}
+		</main>
+	</section>
+{:catch}
+	<div class="error">
+		<h1 class="errorTitle">Houve um erro ao carregar dados dos quiz</h1>
+		<a class="errorLink" href="/" target="_self" rel="next" title="Voltar para home" aria-label="Voltar para home">Voltar para home</a>
+	</div>
+{/await}
 
 <style>
 	section {
@@ -368,7 +392,8 @@
 	}
 
 	.scoredContainerBtn,
-	.quizBtnResponse {
+	.quizBtnResponse,
+	.errorLink {
 		display: flex;
 		flex-flow: row nowrap;
 		align-items: center;
@@ -385,7 +410,8 @@
 	}
 
 	.scoredContainerBtn:is(:hover, :focus),
-	.quizBtnResponse:is(:hover, :focus) {
+	.quizBtnResponse:is(:hover, :focus),
+	.errorLink:is(:hover, :focus) {
 		background: linear-gradient(0deg, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0.5) 100%),
 			var(--purple);
 	}
@@ -511,5 +537,31 @@
 		line-height: 18px;
 		color: var(--choise02);
 		text-align: center;
+	}
+
+	.load, .error{
+		height: 100vh;
+		display: flex;
+		flex-flow: row nowrap;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.loadTitle, .errorTitle{
+		padding: 24px;
+		border-radius: 12px;
+		background-color: var(--choose03);
+		color: var(--choise01);
+		font-weight: 700;
+		font-size: 24px;
+		line-height: 24px;
+	}
+
+	.errorTitle{
+		color: var(--red);
+	}
+
+	.errorLink{
+		text-decoration: none;
 	}
 </style>
